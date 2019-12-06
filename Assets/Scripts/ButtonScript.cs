5970c37266de628c64fff782244fe8c350b29a9c
Varun <varun@hitwicket.com>
2019-12-06 15:43:01 +0530

    Buttons Added1

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameManager gameManager;
+    // Start is called before the first frame update
+
+    public void RestartGame()
+    {
+        gameManager.restart();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
